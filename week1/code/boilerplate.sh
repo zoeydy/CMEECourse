0a6f2219f1e66aa555b1bf14a61a0542ca9f7fec
zongyi <zh2720@imperial.ac.uk>
2020-10-09 11:52:55 +0100

    Full first commit of new project

---
+++ week1/code/boilerplate.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Author: zongyi zh2720@imperial.ac.uk
+# Script: boilerplatesh
+# Desc: simple boilerplate for shell scripts
+# Arguments: none
+# Date: Oct 2019
+
+echo -e  "\nThis is a shell script! \n"
+
+#exit
+
