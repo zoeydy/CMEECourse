2f13e7fe9f3e15b4e1b5be22d5d7c9de0bae5c60
zongyi <zh2720@imperial.ac.uk>
2020-10-15 19:03:54 +0100

    add hiding file in result directory

--- week2/data/Practicals_Comprehensions/dictionary.py
+++ week2/data/Practicals_Comprehensions/dictionary.py
@@ -13,27 +13,12 @@ taxa = [ ('Myotis lucifugus','Chiroptera'),
 # Write a short python script to populate a dictionary called taxa_dic 
 # derived from  taxa so that it maps order names to sets of taxa. 
 # E.g. 'Chiroptera' : set(['Myotis lucifugus']) etc. 
-tax_dic = dict()
+
+taxa_dic = dict()
 for tax in taxa:
-        if tax[1] not in tax_dic.keys():
-                tax_dic[tax[1]] = set([tax[0]])
+        if tax[1] not in taxa_dic.keys():
+                taxa_dic[tax[1]] = set([tax[0]])
         else:
-                tax_dic[tax[1]].add(tax[0])
+                taxa_dic[tax[1]].add(tax[0])
 
-print(tax_dic)
-
-
-# set_delete_repeat_name = set(n[1] for n in taxa)
-# print(set_delete_repeat_name)
-
-# taxa_change = {a:b for a,b in taxa}
-# print(taxa_change)
-
-# def repeat(name):
-#         return name.lower().endswith(set_delete_repeat_name)
-
-# a = (n for n in taxa_change if repeat(name))
-# print(a)
-
-
-#z taxa['merge'] = list(set(eval(taxa['Myotis lucifugus']+','+taxa['b']+taxa)))
+print(taxa_dic)
