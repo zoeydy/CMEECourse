e1a408821713cac59431a0da5975055e258adfdf
zongyi <zh2720@imperial.ac.uk>
2020-10-20 00:15:37 +0100

    updated week2

--- week2/code/basic_io2.py
+++ week2/code/basic_io2.py
@@ -1,11 +1,11 @@
-############
+#############################
 # FILE OUTPUT
-############
-# save the elements of a list to a file
-list_to_sava = range(100)
+#############################
+# Save the elements of a list to a file
+list_to_save = range(100)
 
 f = open('../sandbox/testout.txt','w')
-for i in list_to_sava:
-    f.write(str(i) + '/n') ## Add a new line at the end
+for i in list_to_save:
+    f.write(str(i) + '\n') ## Add a new line at the end
 
-f.close
+f.close()
--- week2/code/basic_io3.py
+++ week2/code/basic_io3.py
@@ -6,15 +6,13 @@ my_dictionary = {"a key": 10, "another key": 11}
 
 import pickle
 
-f = open('../sandbox/testp.p','wb') ## note the b: accept binary files
+f = open('../sandbox/testp.p', 'wb') ## note the b: accept binary files
 pickle.dump(my_dictionary, f)
 f.close()
 
 ## Load the data again
-f = open ('../sandbox/testp.p','rb')
+f = open ('../sandbox/testp.p', 'rb')
 another_dictionary = pickle.load(f)
 f.close()
-
-
 
 print(another_dictionary)
